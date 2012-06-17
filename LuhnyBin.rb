2fe8ff87f1fbfbb566870cb5e742229294ebf6ba
Michael Kohler <michaelkohler@live.com>
2012-06-17 15:59:26 +0200

    Review addressed

--- LuhnyBin.rb
+++ LuhnyBin.rb
@@ -6,91 +6,64 @@ class LuhnyBinChecker
   end
 
   def check
-    createNumberArray
-    doubleSecond
-    sumNumbers
-    getResult
+    arr = digits(@number)
+    puts "Checker: doubling every second number from the right.."
+    arr = doubleSecond(arr)
+    puts "Calculating the sum and validating the result."
+    getResult(sumNumbers(arr))
   end
 
-  def createNumberArray
-    @numberArray = @number.scan(/\d/).map { |c| c.to_i }
+  def digits(n)
+    n.scan(/\d/).map{ |c| c.to_i }
   end
 
   def doubleSecond
-    puts "Checker: doubling every second number from the right.."
-    i = @numberArray.length - 1 
-    pos = 0
-    while i >= 0
-      if pos.modulo(2) == 1
-        @numberArray[i] = @numberArray[i] * 2
-      end
-      i = i - 1
-      pos = pos + 1
+    arr.each_with_index do |item, i|
+      arr[i] = arr[i] * 2 if (i %2 == 0)
     end
   end
 
-  def sumNumbers
-    puts "Checker: calculating the sum.."
-    @sum = 0
-    @numberArray.each do |i|
-      if i > 9
-        @sum = @sum + splittedSum(i) # treat numbers >= 10 individually
-                                     # e.g. 12 -> 1 + 2 = 3
-      else
-        @sum = @sum + i
-      end
-    end
+  # treat numbers >= 10 individually 
+  # e.g. 12 -> 1 + 2 = 3
+  def sumNumbers(arr)
+    arr.map{|i|
+        i > 9 ? splittedSum(i) : i
+    }.inject(0,:+)
   end
 
   def splittedSum(number)
-    numberAsString = number.to_s()
-    splittedNumbers = numberAsString.scan(/\d/).map { |c| c.to_i }
-    splittedSum = 0
-    splittedNumbers.each do |n|
-      splittedSum = splittedSum + n
-    end
-    return splittedSum
+    digits(number).inject(0,:+)
   end
 
-  def getResult
-    if @sum.modulo(10) == 0
-      puts "Credit card number is valid"
-    else
-      puts "Credit card number is not valid"
-    end
+  def getResult(sum)
+    sum.modulo(10) == 0
   end
 end
 
 class CreditCard
+  attr_reader :number
   def setCardNumber(number)
     @cardNumber = number
   end
 
-  def getCardNumber
-    if @cardNumber!= nil
-      @cardNumber
-    end
-  end
-
   def verify
-    puts "Verifying card number now..."
-    checker = LuhnyBinChecker.new(@cardNumber)
-    checker.check
+    (LuhnyBinChecker.new(@number)).check
   end
 end
 
 if __FILE__ == $0
-  card = CreditCard.new
-
   puts "Please enter the credit card number to check:"
-  gets				# input card number
-  input = $_
-  if input.match(/[a-z]/i) != nil || input.match(/\d/) == nil
-    puts "Please enter only numbers."
-    exit
+  input = gets()				# input card number
+  abort "Please enter only numbers." if input !~/(\d+\s)+/
+
+  card = CreditCard.new(input)
+  puts "You entered: " + card.number
+
+  puts "Verifying card number now..."
+   if card.verify()
+     puts "Credit card number is valid"
+  else
+     puts "Credit card number is valid"
   end
 
-  card.setCardNumber(input)
-  puts "You entered: " + card.getCardNumber
-  card.verify
 end
